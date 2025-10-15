eb00ea0138c5eddf06dd273cf20373c18b95ddc4
Phutiane <82708335+PhutianeMichael@users.noreply.github.com>
2025-10-15 19:04:14 +0200

    Initialize application state in AppComponent
    - Added `ngOnInit` lifecycle hook to dispatch initialization actions for authentication and cart state (`appAuthInit`, `appCartInit`).
    - Refactored `AppComponent` to include dependency injection for NgRx store.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { AppState } from './app.state';
+import { appAuthInit } from './feature/auth/state/auth.actions';
+import { Store } from '@ngrx/store';
+import { appCartInit } from './feature/cart/state/cart.actions';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,15 @@ import { HeaderComponent } from './shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'storefront';
+  private store = inject(Store<AppState>)
+
+  /**
+   * Initializes the application state by dispatching auth, cart, and wishlist init actions.
+   */
+  ngOnInit() {
+    this.store.dispatch(appAuthInit());
+    this.store.dispatch(appCartInit());
+  }
 }
